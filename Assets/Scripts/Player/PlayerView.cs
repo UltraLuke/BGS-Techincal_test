827e0536869c25b6dcb685aaf7c8ed8b28c7c879
UltraLuke <42553418+UltraLuke@users.noreply.github.com>
2024-03-09 15:08:40 -0300

    Animations for all outfits
    Every outfit has its animations. Fully tested on character in movement.

--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -1,15 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerView : MonoBehaviour
 {
-    [SerializeField] private Animator[] animators;
+    [SerializeField] private List<Animator> animators;
+
+    private void Awake()
+    {
+        RefreshAnimatorReferences();
+    }
+
+    private void RefreshAnimatorReferences()
+    {
+        animators = GetComponentsInChildren<Animator>().ToList();
+    }
+
+    public void AddAnimator(Animator anim)
+    {
+        if(!animators.Contains(anim))
+            animators.Add(anim);
+    }
 
     public void SetMovementAxes(float h, float v)
     {
-        for (int i = 0; i < animators.Length; i++)
+        for (int i = 0; i < animators.Count; i++)
         {
+            if(!animators[i])
+                continue;
+            
             animators[i].SetFloat("hAxis", h);
             animators[i].SetFloat("vAxis", v);
         }
